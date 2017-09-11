a239642eb0a55e9db1b2d370354569f6c731ae9d
Huigesi <791339970@qq.com>
2017-09-11 15:35:30 +0800

    Initial commit

---
+++ app/src/main/java/cn/gdmec/a071606/myapplication/SplashActivity.java
@@ -0,0 +1,15 @@
+package cn.gdmec.a071606.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        System.out.println("sasasasasas");
+    }
+}
